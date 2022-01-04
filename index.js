78e9eada448ef914209ab64eedad56bd7e25783c
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2022-01-04 13:18:26 -0500

    Attempting to see if port is listening

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const express = require('express');
+morgan = require('morgan');
+const bodyParser = require('body-parser'),
+
 const app = express();
+// utilize morgan
+app.use(morgan('common'));
+app.use(bodyParser.json())
+
+app.get("/movies", (req, res) => {
+    res.json(topMovies);
+});
+
+app.get("/", (req, res) => {
+    res.send("Please enjoy Studio Ghibli");
+});
+app.use(express.static("public"));
+
+app.use((err, req, res, next) => {
+    console,error(err.stack);
+    res.status(500).send("Oops, something is not working right!");
+});
+app.listen(8080, () => {
+    console.log("Your app is lestening on port 8080.")
+});
