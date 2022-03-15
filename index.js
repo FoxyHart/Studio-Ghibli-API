49d87ae9bf8cbaf29a4ce8f70428f87e11e5befd
FoxyHart <88792549+FoxyHart@users.noreply.github.com>
2022-03-15 10:01:52 -0400

    3.6

--- index.js
+++ index.js
@@ -239,10 +239,6 @@ app.use((err, req, res, next) => {
     res.status(500).send("Oops, something is not working right!");
 });
 const PORT = process.env.PORT || 8080;
-/*app.listen('0.0.0.0', () => {
-    console.log('Listening on Port' + port);
-});
-*/
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${ PORT }`);
 });
